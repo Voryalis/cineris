d152a2ba5ce54910a10d2f3b49ca93c10f5586b8
reverence <88051439+Voryalis@users.noreply.github.com>
2026-09-03 03:06:48 +0400

    add engine config

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export const WORLD_WIDTH = 168;
+export const WORLD_HEIGHT = 120;
+export const BLOCK_WIDTH = 14;
+export const BLOCK_HEIGHT = 10;
+export const ROAD_WIDTH = 3;
+export const FONT_SIZE = 16;
+export const LINE_HEIGHT = 18;
+export const MOVE_INTERVAL = 72;
